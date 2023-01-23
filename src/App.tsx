ddda05b16d026252a899480bc874771ed041399d
ryba <karolina@inpeak.pl>
2023-01-23 13:56:24 +0100

    Save values to loclStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+// import CoinChart from "/components/CoinChart";
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="sidebar-container">
         <Sidebar />
       </div>
+      <div className="main-content">{/* <CoinChart /> */}</div>
     </div>
   );
 }
