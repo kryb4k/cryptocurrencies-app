176cb6f389e6c87ece493e7e6c2a19823f4a59fd
ryba <karolina@inpeak.pl>
2023-01-23 14:20:39 +0100

    export CoinChart component

--- src/components/CoinChart.tsx
+++ src/components/CoinChart.tsx
@@ -1,52 +1,52 @@
 // import React, { useEffect, useState } from "react";
 // import { Line } from "react-chartjs-2";
 
-// const CoinChart: React.FC = () => {
-//   const [chartData, setChartData] = useState({});
+const CoinChart: React.FC = () => {
+  //   const [chartData, setChartData] = useState({});
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const res = await fetch(
-//         `https://api.coingecko.com/api/v3/coins/${id}/market_chart/range?vs_currency=usd&from=1577836800&to=1609459200`
-//       );
-//       const data = await res.json();
-//       setChartData({
-//         labels: data.prices.map((price: any) => price[0]),
-//         datasets: [
-//           {
-//             label: "Coin Price",
-//             data: data.prices.map((price: any) => price[1]),
-//             backgroundColor: ["rgba(255, 99, 132, 0.6)"],
-//             borderWidth: 2,
-//             borderColor: "#777",
-//             hoverBorderWidth: 3,
-//             hoverBorderColor: "#000",
-//           },
-//         ],
-//       });
-//     };
+  //   useEffect(() => {
+  //     const fetchData = async () => {
+  //       const res = await fetch(
+  //         `https://api.coingecko.com/api/v3/coins/${id}/market_chart/range?vs_currency=usd&from=1577836800&to=1609459200`
+  //       );
+  //       const data = await res.json();
+  //       setChartData({
+  //         labels: data.prices.map((price: any) => price[0]),
+  //         datasets: [
+  //           {
+  //             label: "Coin Price",
+  //             data: data.prices.map((price: any) => price[1]),
+  //             backgroundColor: ["rgba(255, 99, 132, 0.6)"],
+  //             borderWidth: 2,
+  //             borderColor: "#777",
+  //             hoverBorderWidth: 3,
+  //             hoverBorderColor: "#000",
+  //           },
+  //         ],
+  //       });
+  //     };
 
-//     fetchData();
-//   }, []);
+  //     fetchData();
+  //   }, []);
 
-//   return (
-//     <div>
-//       <Line
-//         data={chartData}
-//         options={{
-//           title: {
-//             display: true,
-//             text: "Coin Price",
-//             fontSize: 25,
-//           },
-//           legend: {
-//             display: true,
-//             position: "right",
-//           },
-//         }}
-//       />
-//     </div>
-//   );
-// };
+  return <div>Chart</div>;
+  //     <div>
+  //       <Line
+  //         data={chartData}
+  //         options={{
+  //           title: {
+  //             display: true,
+  //             text: "Coin Price",
+  //             fontSize: 25,
+  //           },
+  //           legend: {
+  //             display: true,
+  //             position: "right",
+  //           },
+  //         }}
+  //       />
+  //     </div>
+  //   );
+};
 
-// export default CoinChart;
+export default CoinChart;
